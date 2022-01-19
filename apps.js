826900dce63691ce630bfc77326d7bc183308101
Meherazhossain91 <itsonlyopu@gmail.com>
2022-01-19 15:37:52 +0600

    intermidiate to do app

---
+++ apps.js
@@ -0,0 +1,47 @@
+
+
+    const do_form = document.getElementById('do_form');
+
+    const list= document.getElementById('list');
+
+
+    do_form.addEventListener('submit', function(e){
+
+    e.preventDefault();
+
+    let do_name = this.querySelector('input[name="do-name"]').value;
+    let opt = this.querySelector('select[name="opt"]').value;
+
+    if (do_name == '' || opt == ''){
+        alert('All file are required');
+    }else{
+    let li = document.createElement('li');
+    li.className ="list-group-item";
+
+    let closebtn = document.createElement('button');
+    closebtn.innerHTML ='&times;';
+    closebtn.className ='close';
+
+    let dotext = document.createTextNode(do_name);
+
+
+    li.appendChild(closebtn);
+    li.insertBefore(dotext ,closebtn);
+
+    list.appendChild(li); 
+
+    this.querySelector('input[name="do-name"]').value ='';
+    this.querySelector('select[name="opt"]').value = '';
+
+
+    closebtn.addEventListener('click', function(){
+        
+
+        this.parentElement.remove();
+
+
+    })
+    
+    }
+
+    }); 
